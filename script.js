e42368f4223ca972ef12b42f023dff77d47652f0
Anshu kumar <40786793+aanshu-kumar@users.noreply.github.com>
2024-12-18 16:52:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 function makeid(l) {
   // write your code here
-	const character="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-	let charlength=character.length;
+	const character="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0987654321";
+	let char_length=character.length;
 	let result = ' ';
 	for(let i = 0; i <l; i++) {
-            result += character.charAt(Math.floor(Math.random() * charlength));
+            result += character.charAt(Math.floor(Math.random() * char_length));
         }
 	return result;
 }
 
 // Do not change the code below.
-const l = prompt("Enter a number.");
-alert(makeid(l));
+// const l = prompt("Enter a number.");
+// alert(makeid(l));
