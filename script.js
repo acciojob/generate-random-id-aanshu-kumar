a3d07bcf92b6a95d0e8f536c9e98bda2cd1c7385
Anshu kumar <40786793+aanshu-kumar@users.noreply.github.com>
2024-12-18 13:04:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function makeid(l) {
   // write your code here
+	const character="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+	let charlength=character.length;
+	let result = ' ';
+	for(let i = 0; i <l; i++) {
+            result += character.charAt(Math.floor(Math.random() * charlength));
+        }
+	return result;
 }
 
 // Do not change the code below.
